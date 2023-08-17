8539151f6d79bdbdd74d000da711872452ca2d40
meeMuar <105974314+meeMuar@users.noreply.github.com>
2023-08-17 04:55:40 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh     
 sudo git pull https://github.com/meeMuar/pern-ts-tailwind-demo.git
 cd server
-sudo npm install
+npm install
 cd ..
 cd client
 npm install
-sudo npm run build
+npm run build
 cd ..
 sudo systemctl restart nginx
 sudo pm2 restart all
